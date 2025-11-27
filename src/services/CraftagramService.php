3ae103eeafe7fce8ba468f522eefb0c14efa92cb
Bob Olde Hampsink <378974+boboldehampsink@users.noreply.github.com>
2025-11-27 15:24:55 +0100

    Decode data before checking res id (#96)

--- src/services/CraftagramService.php
+++ src/services/CraftagramService.php
@@ -367,11 +367,13 @@ class CraftagramService extends Component {
             $res = curl_exec($ch);
             curl_close($ch);
 
-            if (!isset($res->id)) {
-                Craftagram::info('getInstagramFeed:345: Failed to get data for this $mediaID. Response from Instagram: ' . json_encode($res), 'craftagram');
+            $data = json_decode($res);
+
+            if (!isset($data->id)) {
+                Craftagram::info('getInstagramFeed:345: Failed to get data for this $mediaID. Response from Instagram: ' . $res, 'craftagram');
             }
 
-            $groupedMediaRecords['data'][] = json_decode($res);
+            $groupedMediaRecords['data'][] = $data;
         }
 
         $groupedMediaRecords['paging']['cursors']['before'] = $mediaIDs->paging->cursors->before;
